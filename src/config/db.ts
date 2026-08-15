c2d5ea1487a1e20e47725dc6b62225cd3f081ada
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-15 09:26:24 +0300

    feat: add error handling on database connection

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,9 +2,22 @@ import { Pool } from "pg";
 import "dotenv/config";
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-pool
-  .query("SELECT NOW()")
-  .then((res) =>
-    console.log("PostgreSQL connected succesfully", res.rows[0].now),
-  );
+try {
+  pool
+    .query("SELECT NOW()")
+    .then((res) =>
+      console.log("PostgreSQL connected succesfully", res.rows[0].now),
+    );
+  pool
+    .query(
+      "SELECT table_name FROM information_schema.tables WHERE table_schema NOT IN ('pg_catalog', 'information_schema')AND table_type ='BASE TABLE';",
+    )
+    .then((res) =>
+      res.rows.forEach((row) =>
+        console.log("Tables in the database:", row.table_name),
+      ),
+    );
+} catch (error) {
+  console.log("An error occurred:", error);
+}
 export default pool;
