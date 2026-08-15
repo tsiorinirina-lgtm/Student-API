560315b1e0d3e15fde90990fa09a298b869d96de
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-15 09:48:54 +0300

    build: add migration script file

---
+++ src/scripts/runMigrations.ts
@@ -0,0 +1,20 @@
+import pool from "../config/db.ts";
+import path from "path";
+import { readFileSync, readdirSync } from "fs";
+const runMigration = async () => {
+  try {
+    const migrationsPath = path.join(import.meta.dirname, "../migrations");
+    const files = readdirSync(migrationsPath).sort();
+    for (const file of files) {
+      if (file.endsWith(".sql")) {
+        const filePath = path.join(migrationsPath, file);
+        await pool.query(readFileSync(filePath, "utf-8"));
+        await pool.end();
+      }
+    }
+  } catch (err) {
+    console.log("Error", err);
+    process.exit(1);
+  }
+};
+runMigration();
