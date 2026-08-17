9219ec34513bb12a432ebc76866e0fe1b91c8c59
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-17 16:54:21 +0300

    fix: syntax error in the sql

--- src/migrations/002_create_users_table.sql
+++ src/migrations/002_create_users_table.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS users{
+CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(20),
     email VARCHAR(50),
     password_hash VARCHAR(255) NOT NULL,
     joined_at TIMESTAMP NOT NULL DEFAULT NOW()
-}
+)
